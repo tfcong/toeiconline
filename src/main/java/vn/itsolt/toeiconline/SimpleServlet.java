d6fbb6b5ca37407fc287e8c26d5428f4954ac0de
Phạm Thành Công <congptph05286@fpt.edu.vn>
2018-03-12 00:13:49 +0700

    init project

---
+++ src/main/java/vn/itsolt/toeiconline/SimpleServlet.java
@@ -0,0 +1,25 @@
+package vn.itsolt.toeiconline;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/simple-servlet")
+
+public class SimpleServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out= resp.getWriter();
+        out.println("<h1>Xin chao cac ban</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
